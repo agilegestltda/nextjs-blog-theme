6d8cc97a182fc74fb837d28bd7b625bc1d86e423
agilegestltda <141669828+agilegestltda@users.noreply.github.com>
2023-08-07 15:12:43 -0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'AgileGest Tecnologia';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Esse domínio foi comprado e registrado por BRUNO DA SILVA FREITAS';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Todos os direitos reservados.';
 
   return {
     name,
